dd45f5f64762ca837c7c5ad303b555b90116eb4a
Malassri101 <malassri97@gmail.com>
2019-09-18 00:04:28 +0530

    Initial commit

---
+++ app/src/main/java/com/kasun/tasteit/dashboard.java
@@ -0,0 +1,61 @@
+package com.kasun.tasteit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class dashboard extends AppCompatActivity {
+
+    TextView txtbtn;
+    TextView txtbtn2;
+    TextView txtbtn3;
+    TextView txtbtn4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+        txtbtn = (TextView) findViewById(R.id.arr);
+        txtbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(dashboard.this, category_list.class);
+                startActivity(intent);
+
+            }
+        });
+
+        txtbtn2 = (TextView) findViewById(R.id.arr1);
+        txtbtn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(dashboard.this, medicine_addItems.class);
+                startActivity(intent1);
+            }
+        });
+
+        txtbtn3 = (TextView) findViewById(R.id.arr3);
+        txtbtn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent2 = new Intent(dashboard.this, insert_pet.class);
+                startActivity(intent2);
+            }
+        });
+
+        txtbtn4 = (TextView) findViewById(R.id.dbtnfood);
+        txtbtn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent5 = new Intent(dashboard.this, Food.class);
+                startActivity(intent5);
+            }
+        });
+    }
+}
